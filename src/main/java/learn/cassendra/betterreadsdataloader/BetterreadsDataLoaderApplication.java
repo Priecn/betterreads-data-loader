a19c86f0af42c151e6c65cc139b914bb8aba0476
Priecn <prince.silverarrow@gmail.com>
2021-12-23 18:08:04 +0530

    cassandra connection

--- src/main/java/learn/cassendra/betterreadsdataloader/BetterreadsDataLoaderApplication.java
+++ src/main/java/learn/cassendra/betterreadsdataloader/BetterreadsDataLoaderApplication.java
@@ -1,8 +1,15 @@
 package learn.cassendra.betterreadsdataloader;
 
+import learn.cassendra.betterreadsdataloader.connection.DataStaxAstraConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
+import java.nio.file.Path;
+
+@EnableConfigurationProperties(DataStaxAstraConfig.class)
 @SpringBootApplication
 public class BetterreadsDataLoaderApplication {
 
@@ -10,4 +17,13 @@ public class BetterreadsDataLoaderApplication {
         SpringApplication.run(BetterreadsDataLoaderApplication.class, args);
     }
 
+    /**
+     * This is necessary to have the Spring Boot app use the Astra secure bundle
+     * to connect to the database
+     */
+    @Bean
+    public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraConfig astraProperties) {
+        Path bundle = astraProperties.getSecureConnectBundle().toPath();
+        return builder -> builder.withCloudSecureConnectBundle(bundle);
+    }
 }
